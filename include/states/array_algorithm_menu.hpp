cd6507d2be9f32865f508a857ce486ced58dadf7
zedrex <adib14021999@gmail.com>
2020-09-13 03:07:00 +0600

    Renamed ArraySortMenu to ArrayAlgorithmMenu

---
+++ include/states/array_algorithm_menu.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <states/state.hpp>
+
+class ArrayAlgorithmMenu : public State
+{
+public:
+    ArrayAlgorithmMenu(StateManager *applicationStateManager);
+    ~ArrayAlgorithmMenu();
+};
--- include/states/array_sort_menu.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<states/state.hpp>
-
-class ArraySortMenu: public State{
-    public:
-    ArraySortMenu(StateManager *applicationStateManager);
-    ~ArraySortMenu();
-};
