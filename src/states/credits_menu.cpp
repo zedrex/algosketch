78cec37d83f262cb5cd8f91f602096a0a6873629
zedrex <adib14021999@gmail.com>
2020-09-15 02:51:22 +0600

    Added title to credits menu

--- src/states/credits_menu.cpp
+++ src/states/credits_menu.cpp
@@ -5,6 +5,7 @@
 
 CreditsMenu::CreditsMenu(StateManager *applicationStateManager) : State(applicationStateManager)
 {
+    stateElementList.push_back(new Panel(this->stateManager, 10, 10, 1180, 50, "Credits", 32, sf::Color(200, 200, 200), 0));
     stateElementList.push_back(new Panel(this->stateManager, 10, 10, 1180, 730, "AlgoSketch is created by\nAdib Abrar Kabeer and Shihab Sikder\nas a part of their undergraduate OOP course.\n\nThe project is licensed under the MIT Open Source License", 36, sf::Color(100, 100, 100, 0), 0));
     stateElementList.push_back(new Button(this->stateManager, 902.5, 740, 287.5, 50, "Back", Action::ChangeToMainMenu));
 }
